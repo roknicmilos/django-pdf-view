ef0f3135c384852125b822e9bbc1ee292b5c2605
Miloš Roknić <roknic.milos.994@gmail.com>
2024-06-13 22:04:20 +0200

    Write tests for TestCssTemplateFilter

---
+++ django_pdf/tests/test_css_template_filter.py
@@ -0,0 +1,31 @@
+from unittest.mock import patch, mock_open
+from django.test import SimpleTestCase
+
+from django_pdf.templatetags.css import css
+
+
+class TestCssTemplateFilter(SimpleTestCase):
+
+    @patch(target='django_pdf.templatetags.css.finders.find')
+    def test_css_file_found(self, mock_finders_find):
+        mock_finders_find.return_value = '/path/to/static/file.css'
+        mocked_file_content = 'body { background-color: #fff; }'
+        with patch(
+            target='builtins.open',
+            new=mock_open(read_data=mocked_file_content)
+        ) as mock_file:
+            result = css('file.css')
+            mock_file.assert_called_once_with('/path/to/static/file.css', 'r')
+            self.assertEqual(result, mocked_file_content)
+
+    @patch(
+        target='django_pdf.templatetags.css.finders.find',
+        return_value=None
+    )
+    def test_css_file_not_found(self, _):
+        with self.assertRaises(FileNotFoundError):
+            css('nonexistent.css')
+
+    def test_css_invalid_extension(self):
+        with self.assertRaises(ValueError):
+            css('file.txt')
