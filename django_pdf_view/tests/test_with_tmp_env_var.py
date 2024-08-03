3143bd5591501984b8bac6f2d900a0c3915559e6
Miloš Roknić <roknic.milos.994@gmail.com>
2024-08-03 17:47:33 +0200

    Replace double with single quotes in TestWithTmpEnvVar

--- django_pdf_view/tests/test_with_tmp_env_var.py
+++ django_pdf_view/tests/test_with_tmp_env_var.py
@@ -7,24 +7,24 @@ from django_pdf_view.decorators import with_tmp_env_var
 class TestWithTmpEnvVar(TestCase):
 
     def test_existing_env_var_overridden_during_function_execution(self):
-        os.environ["TEST_ENV_VAR"] = "original_value"
+        os.environ['TEST_ENV_VAR'] = 'original_value'
 
-        @with_tmp_env_var("TEST_ENV_VAR", "temp_value")
+        @with_tmp_env_var('TEST_ENV_VAR', 'temp_value')
         def test_function():
-            self.assertEqual(os.environ["TEST_ENV_VAR"], "temp_value")
+            self.assertEqual(os.environ['TEST_ENV_VAR'], 'temp_value')
 
         test_function()
 
-        self.assertEqual(os.environ["TEST_ENV_VAR"], "original_value")
+        self.assertEqual(os.environ['TEST_ENV_VAR'], 'original_value')
 
     def test_new_env_var_set_during_function_execution(self):
-        if "TEST_ENV_VAR" in os.environ:
-            del os.environ["TEST_ENV_VAR"]
+        if 'TEST_ENV_VAR' in os.environ:
+            del os.environ['TEST_ENV_VAR']
 
-        @with_tmp_env_var("TEST_ENV_VAR", "temp_value")
+        @with_tmp_env_var('TEST_ENV_VAR', 'temp_value')
         def test_function():
-            self.assertEqual(os.environ["TEST_ENV_VAR"], "temp_value")
+            self.assertEqual(os.environ['TEST_ENV_VAR'], 'temp_value')
 
         test_function()
 
-        self.assertNotIn("TEST_ENV_VAR", os.environ)
+        self.assertNotIn('TEST_ENV_VAR', os.environ)
